916b86d0acf9ff158c30eb9d8656168618e88b8e
sirgrantt <sirgrantt@gmail.com>
2019-08-25 15:26:36 -0500

    Added ability to grab the branch name

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bytes"
-	"flag"
+	//	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	//firstArg := flag.String("first", "", "the first value in the world")
-	flag.Parse()
+	// flag.Parse()
 
 	// if *firstArg == "" {
 	// 	fmt.Println("hey, give me a value")
@@ -19,16 +19,31 @@ func main() {
 	// 	os.Exit(1)
 	// }
 
-	cmd := exec.Command("Get Git Branch")
-	cmd.Stdin = strings.NewReader("git branch | grep \\* | cut -d ' ' -f2")
+	// cmd := exec.Command("git", "branch | grep \\* | cut -d ' ' -f2")
+	// args := []string{"branch", "|", "grep", "\\*", "cut", "-d", "' '", "-f2"}
+	cmd := exec.Command("git", "branch")
+	//cmd.Stdin = strings.NewReader("branch | grep \\* | cut -d ' ' -f2")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
+
+	result := strings.Split(out.String(), "\n")
+
+	var branchName string
+
+	for i, val := range result {
+		found := strings.Index(val, "*")
+		if found > -1 {
+			selectedBranch := result[i]
+			splitFromStar := strings.Split(selectedBranch, "*")
+			branchName = strings.TrimSpace(splitFromStar[1])
+		}
+	}
 
 	if err != nil {
 		fmt.Println("Error running command")
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-	fmt.Println(out.String())
+	fmt.Println(branchName)
 }
